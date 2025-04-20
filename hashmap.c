82d896d488441af2d88dfab8f21077c4b2bfffc5
Your Name <you@example.com>
2025-04-20 15:35:00 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -126,10 +126,15 @@ Pair * firstMap(HashMap * map) {
 }
 
 Pair * nextMap(HashMap * map) {
-    //if (map == NULL) return NULL;
+    if (map == NULL) return NULL;
 
+    for (long i = map->current = +1; i < map->capacity; i++) {
+        if (map->buckets[i] != NULL && map->buckets[i]->key != NULL) {
+            map->current = i;
+            return map->bucktets[i];
+        }
+    } 
     
-    
-
+    map->current = 0;
     return NULL;
 }
